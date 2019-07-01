c63a6bd66167077649d285c2d55c8b6ba3b175b0
Simonas Karuzas <simonas@not.cat>
2019-07-01 11:38:50 +0300

    Delayed response solved

--- actions/tos.ts
+++ actions/tos.ts
@@ -1,16 +1,29 @@
 import { slackInteractions } from './adapter'
+const { WebClient } = require('@slack/web-api')
+import { prisma } from '../generated/prisma-client'
 
-slackInteractions.action('accept_tos', (payload, respond) => {
+slackInteractions.action('accept_tos', async (payload, respond) => {
   console.log({payload})
   console.log(`The user ${payload.user.name} in team ${payload.team.domain} pressed a button`);
 
-    respond({
-      text: 'Thank you for selecting: ' + payload.actions[0].value
-    });
+  respond({
+    text: 'Thank you for selecting: ' + payload.actions[0].value
+  })
 
-  // Before the work completes, return a message object that is the same as the original but with
-  // the interactive elements removed.
-  const reply = payload.original_message
-  delete reply.attachments[0].actions
-  return reply
-});
+  const installations = await prisma.installations({where: {team_id: payload.team.id}})
+  console.log({installations})
+  const web = new WebClient(installations[installations.length - 1].access_token)
+  try {
+    const result = await web.chat.postMessage({
+      text: 'Hello worldas!',
+      channel: payload.channel.id,
+    });
+  } catch (e) {
+    console.log(e)
+  }
+
+  respond({
+    delete_original: true
+  })
+
+})
